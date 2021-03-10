2995aec0330013f02aee8fcac1fb488a20223e65
heronet <sirat4757@gmail.com>
2021-03-10 20:16:21 +0600

    binarySearch.cc update

--- binarySearch.cc
+++ binarySearch.cc
@@ -18,6 +18,23 @@ int binarySearch(std::vector<int> arr, int num) {
 }
 
 int main() {
-    std::cout << binarySearch({1, 3, 5, 7, 9}, 5) << std::endl;
+    std::size_t length;
+    std::cout << "How many numbers do you want the array to have? ";
+    std::cin >> length;
+    std::cout << "Okay! Enter them one by one: ";
+    std::vector<int> input;
+    for(int i = 0; i != length; ++i) {
+        int in;
+        std::cin >> in;
+        input.push_back(in);
+    }
+    std::cout << "Done! Now Enter the number you want to find: ";
+    int num_to_look;
+    std::cin >> num_to_look;
+    int result = binarySearch(input, num_to_look);
+    if(result != -1)
+        std::cout << "We found it at index " << result << std::endl;
+    else
+        std::cout << "We couldn't find it :-( Index " << result << std::endl;
     return 0;
 } 
