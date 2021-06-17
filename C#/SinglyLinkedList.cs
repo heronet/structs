493730efd5f69ddb253a27e6dbd0ecb434922ad7
heronet <sirat4757@gmail.com>
2021-06-17 09:30:28 +0600

    SL C#

---
+++ C#/SinglyLinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    public class SinglyLinkedList<T>
+    {
+        public class Node<TT>
+        {
+            public TT Value { get; set; }
+            public Node<TT> Next { get; set; } = null;
+            public Node(TT value) => Value = value;
+        }
+        public Node<T> Head { get; set; } = null;
+        public Node<T> Tail { get; set; } = null;
+        public int Size { get; set; }
+
+        public void Push(T value)
+        {
+            var node = new Node<T>(value);
+            if(Head == null)
+            {
+                Head = node;
+                Tail = Head;
+            } else
+            {
+                Tail.Next = node;
+                Tail = Tail.Next;
+            }
+            ++Size;
+        }
+        public void Print()
+        {
+            List < T > = new List<T>();
+            var current = Head;
+            while(current != null)
+            {
+                Console.WriteLine(current.Value);
+                current = current.Next;
+            }
+        }
+        public void Reverse()
+        {
+            var node = Head;
+            Head = Tail;
+            Tail = node;
+            Node<T> previous = null;
+
+            for(int i = 0; i != Size; ++i)
+            {
+                Node<T> next = node.Next;
+                node.Next = previous;
+                previous = node;
+                node = next;
+            }
+        }
+    }
+}
